1c1ed7db1aaba8eab86efa00aaaab7e4fb394e94
szapeta <2375894860101@ingenieria.usac.edu.gt>
2026-08-09 23:22:19 -0600

    detalle del tercer turno

---
+++ script.js
@@ -0,0 +1,33 @@
+const datos = {
+    coordinador: 'Sergio',
+    filas: [
+        ['Intenciones', 'Melany'],
+        ['Moniciones', 'Antonio'],
+        ['1ra.', 'Cesar'],
+        ['Salmo', 'Jazmin'],
+        ['2da.', 'Estela'],
+        ['O.F.', 'Marta'],
+        [
+            'Ofrendas',
+            [
+                'Ana',
+                'Armando',
+                'Cristina',
+                'Emily',
+                'Justin',
+                'Marlon',
+                'Mayerly',
+                'Rosa'
+            ]
+        ]
+    ]
+};
+c = document.getElementById('contenido');
+datos.filas.forEach(f => {
+    let d = document.createElement('div');
+    d.className = 'fila';
+    let of = Array.isArray(f[1]);
+    d.innerHTML = `<div class="serv">${f[0]}</div><div class="nombre ${of ? 'ofrendas' : ''}">${of ? f[1].join(' • ') : f[1]}</div>`;
+    c.appendChild(d);
+});
+document.getElementById('coord').textContent = datos.coordinador;
